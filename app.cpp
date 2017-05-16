3b8d52be51cde7592b2a7ecb2a68a3bde03a67c1
Jarek Páral <paral.jarek@gmail.com>
2017-05-16 19:32:41 +0200

    Template project

---
+++ app.cpp
@@ -0,0 +1,18 @@
+/**
+ * Template for your project (led blinking).
+ *
+ * Author: Jaroslav Páral (jarekparal)
+ */
+
+#include <cstdlib>
+
+#include "ev3cxx.h"
+#include "app.h"
+
+using ev3cxx::display;
+using ev3cxx::format;
+    
+void main_task(intptr_t unused) {
+    // Paste your code here
+
+}
